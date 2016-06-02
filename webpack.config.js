5511b0138efcbad0c29e80e618d3470b853de133
gactocat <gactocat@gmail.com>
2016-06-02 20:18:16 +0900

    apply airbnb checkstyle.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-    entry: {
-        jsx: "./src/index.jsx",
-        html: "./src/index.html",
-    },
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js",
-    },
-    module: {
-        preLoaders: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"}
-        ],
-        loaders: [
-            { test: /\.html$/, loader: "file?name=[name].[ext]" },
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
-    }
+  entry: {
+    jsx: './src/index.jsx',
+    html: './src/index.html',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    preLoaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'eslint-loader' },
+    ],
+    loaders: [
+      { test: /\.html$/, loader: 'file?name=[name].[ext]' },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
+  },
+  eslint: {
+    configFile: './.eslintrc.json',
+  },
 };
